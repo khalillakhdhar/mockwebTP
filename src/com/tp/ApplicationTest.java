2ffd854560df71bc4f9d4a474ab6b7930bf965b2
Khalil Lakhdhar <khalillakhdharatc@gmail.com>
2021-05-27 12:44:32 +0100

    test preparation

--- src/com/tp/ApplicationTest.java
+++ src/com/tp/ApplicationTest.java
@@ -1,5 +1,31 @@
 package com.tp;
 
+import org.junit.Before;
+import org.junit.Test;
+import org.mockito.InjectMocks;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.tp.dao.DatabaseDao;
+import com.tp.services.InscriptionService;
+
 public class ApplicationTest {
+@Before
+public void initMocks()
+{
+	MockitoAnnotations.initMocks(this);
+
+}
+@InjectMocks
+InscriptionService inscription;
+@Mock
+DatabaseDao stub;
+@Test
+public void addUser()
+{
+	
+
+}
+
 
 }
